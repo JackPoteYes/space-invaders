c43683f8a66f65e9f90980233880782aa5a79c93
JackPote <guilhem.forey@hotmail.fr>
2019-12-28 19:42:10 +0000

    My ship can move

--- index.js
+++ index.js
@@ -1,14 +1,96 @@
 var ROOT_COMPONENT = document.getElementById("root");
 var MARGIN_BOTTOM_PERCENT = 10;
+var MOVE_SPEED = 1;
 
-init();
+var CURRENTLY_MOVING = false;
+
+var MY_SHIP = null;
+var ANIMATION_INTERVAL = null;
+var ANIMATION_PERIOD = 20;
+
+/**
+ * INITIALISATION
+ */
+
+var CANVAS_WIDTH = null;
+var SHIP_WIDTH = null;
+var SHIP_WIDTH_PERCENTAGE = null;
+
+window.onload = init;
 
 function init() {
-  const myShip = document.createElement("div");
-  myShip.id = "myShip";
-  myShip.style.bottom = percent(MARGIN_BOTTOM_PERCENT);
-  root.appendChild(myShip);
+  MY_SHIP = document.createElement("div");
+  MY_SHIP.id = "myShip";
+  MY_SHIP.style.bottom = percent(MARGIN_BOTTOM_PERCENT);
+  MY_SHIP.style.left = percent(0);
+  ROOT_COMPONENT.appendChild(MY_SHIP);
+  CANVAS_WIDTH = ROOT_COMPONENT.offsetWidth;
+  SHIP_WIDTH = MY_SHIP.offsetWidth;
+  SHIP_WIDTH_PERCENTAGE = (SHIP_WIDTH * 100) / CANVAS_WIDTH;
 }
+
+/**
+ * Controls
+ */
+
+function startMove(direction) {
+  CURRENTLY_MOVING = direction;
+  ANIMATION_INTERVAL = setInterval(() => {
+    const currentLeftPercentage = parseFloat(MY_SHIP.style.left, 10);
+    const addedValue =
+      direction === "left"
+        ? currentLeftPercentage <= 0
+          ? 0
+          : -MOVE_SPEED
+        : currentLeftPercentage >= 100.0 - SHIP_WIDTH_PERCENTAGE
+        ? 0
+        : MOVE_SPEED;
+    MY_SHIP.style.left = percent(currentLeftPercentage + addedValue);
+  }, ANIMATION_PERIOD);
+}
+
+function stopMove() {
+  CURRENTLY_MOVING = false;
+  clearInterval(ANIMATION_INTERVAL);
+}
+
+/**
+ * EVENTS
+ */
+
+document.addEventListener("keydown", event => {
+  switch (event.keyCode) {
+    // Right
+    case 39:
+      if (CURRENTLY_MOVING === "right") break;
+      stopMove();
+      startMove("right");
+      break;
+    // Left
+    case 37:
+      if (CURRENTLY_MOVING === "left") break;
+      stopMove();
+      startMove("left");
+      break;
+  }
+});
+
+document.addEventListener("keyup", event => {
+  const keyCode = event.keyCode;
+  switch (keyCode) {
+    // Right
+    case 39:
+      if (CURRENTLY_MOVING === "right") stopMove();
+      break;
+    // Left
+    case 37:
+      if (CURRENTLY_MOVING === "left") stopMove();
+      break;
+  }
+  if (!CURRENTLY_MOVING && (keyCode === 39 || keyCode === 37)) {
+    stopMove();
+  }
+});
 
 /**
  * Utils
